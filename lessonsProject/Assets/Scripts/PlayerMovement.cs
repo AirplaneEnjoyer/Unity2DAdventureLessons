fb60cbd86f543c05bf7c1ecb03dd55f14e68fae0
AirplaneEnjoyer <2239378@talnet.nl>
2026-03-31 11:41:05 +0200

    Player Movement
    Player Movement aangemaakt en player animations

---
+++ lessonsProject/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using static UnityEngine.UIElements.UxmlAttributeDescription;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+    private Animator anim;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
+
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+
+
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        if (Input.GetKey(KeyCode.Space))
+            body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
+
+        anim.SetBool("Run", horizontalInput != 0);
+    }
+}
